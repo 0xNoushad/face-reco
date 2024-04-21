31ed28ee6dc92c61c35942a73a429b4a0b8deb46
0xNoushad <choudharynoushad46@gmail.com>
2024-04-22 00:09:11 +0530

    work

---
+++ 6/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FaceRecognition from './FaceRecognition';
+
+const App = () => {
+  return (
+    <div>
+      <FaceRecognition />
+    </div>
+  );
+};
+
+export default App;
